a14f8b02956213b78c8a345d1fdccade6d2b701f
Vinicius Valente <viniciusvramirez0108@gmail.com>
2025-06-15 18:13:21 -0300

    Added data validation with spring-boot-starter-validation

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,8 +1,11 @@
 package academy.devdojo.springboot2.requests;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class AnimePostRequestBody {
+    @NotEmpty(message = "The name of this anime cannot be empty")
     private String name;
 }
